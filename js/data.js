1732f36e4b72b81cf6642bb2eabc9e2c0a2a8683
isabelle may salmon <845868806@bun2-206-13.local>
2018-06-13 08:51:52 +1200

    First commit set up

---
+++ js/data.js
@@ -0,0 +1,22 @@
+var accomodation = {
+    hotel: {
+        capacity: [1,2],
+        duration: [1,2,3,4,5],
+        price: 157
+    },
+    hostel: {
+        capacity: [1,2,3,4,5,6],
+        duration: [1,2,3,4,5,6,7,8,9,10],
+        price: 30
+    },
+    motel: {
+        capacity: [2,3,4],
+        duration: [3,4,5,6,7,8,9,10],
+        price: 90
+    },
+    house: {
+        capacity: [1,2],
+        duration: [2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+        price: 240
+    }
+}
